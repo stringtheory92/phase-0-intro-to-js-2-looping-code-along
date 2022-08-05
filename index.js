574613d66b310e98516b9d2fa55054ad5eef7ad7
Adam Kabak <adamkabak@yahoo.com>
2022-08-04 22:42:22 -0400

    commit tree b0b3c8f1e7bda2a9d8f39877c4247ff09d1e8f5a

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+}*/
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+const messages = [];
+
+/*function wrapGifts(gifts) {
+    let a = 0;
+    while (a < gifts.length) {
+        console.log(`Wrapped ${gifts[a]} and addad a bow!`);
+        a++;
+    }
+}
+/*function wrapGifts(gifts) {
+    for (let a = 0; a < gifts.length; a++) {
+        console.log(`Wrapped ${gifts[a]} and added a bow!`);
+        
+    }
+
+   return gifts;
+}*/
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        messages[i] = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
+    }
+    console.log(messages);
+    return messages;
+}
+
+
+writeCards(["Bob", "Joe", "Sam"], "birthday");
+
+function countDown(x) {
+
+    while ( parseInt(x) >= 0) {
+        console.log(parseInt(x));
+        x--;
+    }
+
+}                   
+
+countDown(10);
